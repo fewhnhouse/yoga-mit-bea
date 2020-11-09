abc5d676686da1fb0f84038c3796b9beb8126004
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2020-11-09 10:26:23 +0000

    adjust api server call

--- src/pages/podcasts/Podcasts.svelte
+++ src/pages/podcasts/Podcasts.svelte
@@ -15,7 +15,7 @@
   const { apiUrl } = process.env;
   const t0 = performance.now();
 
-  axios.get(`https://yoga-mit-bea-server.fewhnhouse.now.sh/api/podcasts`).then(res => {
+  axios.get(`https://yoga-mit-bea-server.fewhnhouse.vercel.app/api/podcasts`).then(res => {
     const t1 = performance.now();
     podcasts = res.data.map(podcast => ({
       ...podcast,
