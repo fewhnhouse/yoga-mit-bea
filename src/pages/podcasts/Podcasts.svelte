5fd31d448b558f3d41570865f7daaa075c267e15
Felix Wohnhaas <felix@pricenow.ch>
2021-11-28 21:33:51 +0100

    fix: yoga online

--- src/pages/podcasts/Podcasts.svelte
+++ src/pages/podcasts/Podcasts.svelte
@@ -79,8 +79,8 @@
 {:else}
   <div class="outerContainer">
     <h1>Yoga Online</h1>
-    <!-- <h4>Ab 01.12.2020, Dienstag von 09:30-10:30 und von 20:00-21:00 Uhr</h4> -->
-    <h4>Derzeit findet kein Online-Yoga statt.</h4>
+    <h4>Ab jetzt immer Montags von 18:00-19:50 Uhr und Mittwochs von 20:15-21:15 Uhr</h4>
+    <!-- <h4>Derzeit findet kein Online-Yoga statt.</h4> -->
     <div class="">
       <Text>
         Drücke
